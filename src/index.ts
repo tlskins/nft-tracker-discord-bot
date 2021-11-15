d1835ca48091db9ac572498db87ce010bb852726
TimothyLee19 <>
2021-11-15 02:39:50 +0000

    add repl support

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import path from "path";
 import { CoreClient } from "discord-bot-core-client";
+const keepAlive = require("./server")
 
 const client = new CoreClient({
   token: process.env.DISCORD_BOT_TOKEN as string,
 });
+
+keepAlive()
 
 client.registerBotsIn(path.resolve(__dirname, "bots")).start();
